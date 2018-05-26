3a130fedac6cd586efd18078013fa6f399597f89
linglian <linglian@aliyun.com>
2018-05-27 01:25:17 +0800

    d

--- run_evaluate.sh
+++ run_evaluate.sh
@@ -1,10 +1,11 @@
 MODELS_PATH="/home/slu/tasks/models_02/research/slim"
 TRAIN_DIR="/www/clothes_train_tf"
+TEST_DIR="/www/clothes_test_tf"
 
 cd $MODELS_PATH
 
 python eval_image_classifier.py \
-    --dataset_dir=${TRAIN_DIR} \
+    --dataset_dir=${TEST_DIR} \
     --dataset_name=buliao \
     --num_samples=481448 \
     --num_classes=100000 \
