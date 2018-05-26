dd22cad1c331bb4e7f96e05cab2d1495a21e2630
linglian <linglian@aliyun.com>
2018-05-26 22:35:04 +0800

    b

--- run_evaluate.sh
+++ run_evaluate.sh
@@ -1,15 +1,12 @@
 MODELS_PATH="/home/slu/tasks/models_02/research/slim"
 TRAIN_DIR="/www/clothes_train_tf"
 
-
 cd $MODELS_PATH
 
-python train_image_classifier.py \
-    --train_dir=${TRAIN_DIR}/logs \
+python eval_image_classifier.py \
     --dataset_dir=${TRAIN_DIR} \
+    --dataset_name=buliao \
     --num_samples=481448 \
     --num_classes=100000 \
     --model_name=resnet_v1_50 \
-    --checkpoint_path=/home/slu/tasks/resnet_v1_50.ckpt \
-    --checkpoint_exclude_scopes=resnet_v1_50/logits \
-    --learning_rate=0.01
+    --checkpoint_path=${TRAIN_DIR}/logs \
