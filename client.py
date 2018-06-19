cb908866eabf7ef80c731383a93dc45c8a9412a1
Elle-Peng <374237925@qq.com>
2018-06-19 16:00:46 +0800

    123

---
+++ client.py
@@ -0,0 +1,31 @@
+
+from multiprocessing.connection import Client
+from multiprocessing.connection import Listener
+
+resp = {
+    'info': '',
+    'body': {}}
+
+img = '/media/lee/data/macropic/宏观分类自采/格子/1201/DSC03471.JPG'
+c = Client('/usr/local/server%d.temp' % 99, authkey=b'lee123456')
+# 将信息传送给服务端
+c.send(['-f', img])
+# 等待服务端处理结果
+ar = c.recv()
+is_Ok = False
+t_idx = 0
+for idx, i in enumerate(ar):
+    if i == 'Next':
+        is_Ok = True
+    elif is_Ok:
+        t_idx = idx
+        break
+class_name_list = ar[t_idx]
+file_path_list = ar[t_idx + 1]
+feature_list = ar[t_idx + 2]
+featrue = ar[t_idx + 3]
+resp['body']['class_name_list'] = class_name_list
+resp['body']['file_path_list'] = file_path_list
+resp['body']['feature_list'] = feature_list
+resp['body']['featrue'] = featrue
+print resp
