9fe86c74a702518e45ef02d0be11f917228220e9
Elle-Peng <374237925@qq.com>
2018-06-25 23:20:55 +0800

    asd

--- run_fintune.sh
+++ run_fintune.sh
@@ -6,9 +6,9 @@ cd $MODELS_PATH
 python train_image_classifier.py \
     --train_dir=${TRAIN_DIR}/logs \
     --dataset_dir=${TRAIN_DIR} \
+    --dataset_split_name=list_tfrecord \
     --num_samples=43769 \
     --num_classes=100000 \
     --model_name=resnet_v1_50 \
-    --checkpoint_path=/home/slu/tasks/resnet_v1_50.ckpt \
     --checkpoint_exclude_scopes=resnet_v1_50/logits \
     --learning_rate=0.01
