dbd0eaa4d2bfa5dce6e832ee5c00e71bffd8b0b3
Elle-Peng <374237925@qq.com>
2018-06-27 16:32:58 +0800

    asd

--- run_fintune.sh
+++ run_fintune.sh
@@ -1,5 +1,5 @@
-MODELS_PATH="/home/lee/DeepLearn/models/research/slim"
-TRAIN_DIR="/media/lee/data/macropic/newp/train_tf"
+MODELS_PATH="/home/slu/tasks/models_02/research/slim"
+TRAIN_DIR="/www/linglian_test_data"
 
 cd $MODELS_PATH
 
@@ -9,7 +9,7 @@ python train_image_classifier.py \
     --dataset_split_name=list_tfrecord \
     --num_samples=43769 \
     --num_classes=100000 \
-    --batch_size=8 \
+    --batch_size=32 \
     --model_name=resnet_v1_50 \
     --checkpoint_exclude_scopes=resnet_v1_50/logits \
     --learning_rate=0.01
